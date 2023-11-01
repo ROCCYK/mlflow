0a4169e7932e16bf05d3a33411f02487974bda0e
Rhichard Koh <82534598+ROCCYK@users.noreply.github.com>
2023-11-01 19:35:30 -0400

    3nd commit

--- 1.MlFlow_Intro.py
+++ 1.MlFlow_Intro.py
@@ -71,6 +71,8 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
+        remote_server_url = "https://dagshub.com/ROCCYK/mlflow.mlflow"
+        mlflow.set_tracking_url(remote_server_url)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
